653b0ea942becb8098494cd69d95f1ea478d1340
danargolo <dan.argolo@gmail.com>
2024-02-09 01:32:59 -0300

    style: adjust hamburger transition

--- src/components/_HamburgerButton/styles.js
+++ src/components/_HamburgerButton/styles.js
@@ -1,106 +1,57 @@
 import styled, { keyframes } from "styled-components";
 
-export const AnimationSpan1 = keyframes`
-  0% {
-    transform: rotate(0deg);
-  /* -webkit-transform: rotate(0deg);
-  -moz-transform: rotate(0deg);
-  -o-transform: rotate(0deg);
-  transform: rotate(0deg); */
-  }
-
-  100% {
-    top: 17px;
-    /* transform: translateY(-17px); */
-    transform: rotate(-135deg);
-      /* -webkit-transform: rotate(-135deg);
-      -moz-transform: rotate(-135deg);
-      -o-transform: rotate(-135deg);
-      transform: rotate(-135deg); */
-  }
-`
-export const AnimationSpan2 = keyframes`
-  0% {
-
-  }
-
-100% {
-    height: 0;
-    opacity: 0;
-  }
-`
-export const AnimationSpan3 = keyframes`
-  0% {
-    transform: rotate(0deg);
-  /* -webkit-transform: rotate(0deg);
-  -moz-transform: rotate(0deg);
-  -o-transform: rotate(0deg);
-  transform: rotate(0deg); */
-  }
-
-100% {
-  bottom: 8px;
-  transform: rotate(135deg);
-  /* -webkit-transform: rotate(135deg);
-  -moz-transform: rotate(135deg);
-  -o-transform: rotate(135deg);
-  transform: rotate(135deg); */
-  }
-`
-
 export const HamburgerBtn = styled.button`
   display: flex;
   flex-direction: column;
-  width: 3.125rem;
-  height: 3.125rem;
-  /* border: 2px solid red; */
-  background: transparent;
-  gap: 6px;
+  width: 1.9rem;
+  height:  1.9rem;
+  border: none;
+  background: none;
+  outline: none;
   z-index: 0;
 `
 
 export const SpanMenu = styled.span`
   display: block;
   position: relative;
-  width: 2.8rem;
-  height: .5rem;
+  width: 1.875rem;
+  height: .1875rem;
   margin: .0625rem;
   background-color: red;
-  border-radius: .25rem;
-  transition: all 0.25s ease-in-out 0s;
+  border-radius: .15rem;
+  transform-origin: left center;
+  transition: all 0.5s ease-in-out;
 
 
   &.Span1 {
     ${(props) =>
       props.$active &&
       `
-        transform: translateX(10px) translateY(4px) rotate(47deg);
-        transform-origin: left top;
-      `}
+        transform: rotate(45deg);
+        top: -0.025rem; !important
+        `}
+    top: 0
   }
+
   &.Span2 {
     ${(props) =>
       props.$active &&
       `
-        height: 0;
+        width: 0;
         opacity: 0;
       `}
+    top: .3125rem
   }
 
   &.Span3 {
     ${(props) =>
       props.$active &&
       `
-        transform: translateX(3px) translateY(13px) rotate(-47deg);
-        transform-origin: left top;
-      `}
+        transform: rotate(-45deg);
+        top: .67rem; !important
+        `}
+    top: .625rem
   }
-  /* &.Span2 {
-    animation: ${(props) => (props.$active ? AnimationSpan2 : null)} .5s forwards;
-  } */
-  /* &.Span3 {
-    animation: ${(props) => (props.$active ? AnimationSpan3 : null)} .5s forwards;
-  } */
 `
 
 

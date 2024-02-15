e96f20d9b591a5aec0cf2f4abc2c3558ee79628e
danargolo <dan.argolo@gmail.com>
2024-02-15 14:54:30 -0300

    feat: add component Projects

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+
+export const Projects = () => {
+  return(
+
+    <S.Projects>
+      <S.ProjectsWrapper>
+        <S.Text>Projetos</S.Text>
+      </S.ProjectsWrapper>
+    </S.Projects>
+  )
+}
+

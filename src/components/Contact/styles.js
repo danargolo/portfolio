28011c6b5313d414e6047c9660c2528ab9fe2409
danargolo <dan.argolo@gmail.com>
2024-03-04 20:29:45 -0300

    feat: partial Contact component

--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -1,6 +1,7 @@
 "use client"
 
 import styled from "styled-components"
+import { Ulist } from "../ui/Ulist"
 
 export const Text = styled.p`
   margin-top: 2.5rem;
@@ -15,9 +16,26 @@ export const Contact = styled.section`
   justify-content: center;
   align-items: center;
   width: 100vw;
-  height: fit-content;
+  min-height: 25.625rem;
+  /* height: fit-content; */
   gap: 3rem;
   background-color: ${({theme}) => theme.colors.bg100};  
   position: relative;
   z-index: 1;
+
+  & p {
+    margin: 20px;
+  }
+`
+export const  ContactWrapper = styled.div`
+`
+
+export const StyledUl = styled(Ulist)`
+  display: flex;
+  width: 65%;
+  justify-content: space-between;
+
+  & li {
+    color: ${({ theme }) => theme.colors.text100 };
+  }
 `

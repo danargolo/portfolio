240e25eec84e8afb1abbff6dd493b6805a1b7c84
danargolo <dan.argolo@gmail.com>
2024-03-25 02:58:34 -0300

    feat: add desktop top button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,13 @@ import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { Projects } from '@/components/ProjectsPreviews'
+import { TopButton } from '@/components/ui/BackToTop'
+
 
 export default function Home() {
   return (
     <>
+      <TopButton />
       <Header />
       <Hero />
       <Projects />
---
+++ src/components/ui/BackToTop/index.jsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useEffect, useRef, useState } from "react"
+import ScrollLink from '@/utils/link'
+import * as S from './styles'
+import Image from "next/image";
+import chevron from "../../../../public/icons/chevron-up.svg"
+// import Image from "next/image";
+// import chevron from "../../public/assets/icons/icons8-chevron-50-up.png";
+
+/* Tentar calcular o minimo scrollPosition de acordo com tamanho da tela*/
+
+export const TopButton = ({className}) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    if (window.scrollY >= (5 * document.documentElement.clientHeight)/100) {
+      setIsVisible(true)
+    }
+    else {
+      setIsVisible(false)
+    }
+  }, [scrollPosition]);
+  
+  return(
+    <S.DivWrapper className={className} number={2} teste={isVisible}>
+      <S.StyledScrollLink href="#header">
+        <S.StyledImage src={chevron} width={45} heigth={45}/>
+      </S.StyledScrollLink>
+    </S.DivWrapper>
+  )
+};
